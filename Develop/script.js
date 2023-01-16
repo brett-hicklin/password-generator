c0c0ecc29cfb8f0dfb84300b3fb04b3aed7d64cc
Brett Hicklin <hicklin.brett@gmail.com>
2023-01-16 16:45:55 -0500

    determined what type of character to generate

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,17 @@
 // Assignment Code
 // goes to HTML doc and finds the generate ID inside of button
 var generateBtn = document.querySelector("#generate");
+var alphabet = ['a', 'b', 'c', 'd' ,'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var symobols = ['!','"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
 
-var passwordLength = 8;
-var isLowercase = true;
-var isUppercase = false;
-var isNumeric = false;
-var isSpecialChar = false;
-var passwordReqs = getPasswordReqs();
 
+
+// collects the data from which character requirements will be used for password, returns as array.
 function getPasswordReqs(){
+  var isLowercase = true;
+  var isUppercase = true;
+  var isNumeric = false;
+  var isSpecialChar = false;
   var requirements = []
   if (isLowercase){
     requirements.push('lowercase')
@@ -32,10 +34,29 @@ function getRandomInt(max){
 
 
 function generatePassword(){
+  var passwordLength = 8;
+  // var passwordReqs = getPasswordReqs();
+  var passwordReqs = ['uppercase']
 
   for (let i=0; i < passwordLength; i++ ){
-    getRandomInt()
-    return 
+    var passwordReqIndex = getRandomInt(passwordReqs.length)
+    if (passwordReqs[passwordReqIndex] === 'lowercase'){
+
+    }
+
+    if (passwordReqs[passwordReqIndex] === 'uppercase'){
+      
+    }
+
+    if (passwordReqs[passwordReqIndex] === 'numeric'){
+      
+    }
+    
+    if (passwordReqs[passwordReqIndex] === 'specialChar'){
+      
+    }
+
+  console.log(passwordReqIndex)
   }
 }
 
@@ -56,6 +77,15 @@ generateBtn.addEventListener("click", writePassword);
   create password length prompt
   create character prompt 
 
+  For each character in the password 
+Determine what type of character (lowercase, uppercase, number, symbol)
+Randomly select that type of character 
+    If that character type is lowercase or uppercase, generate random num between 0-25, gran associated letter
+        If lowercase call toLowercase
+        If uppercase call toUppercase
+    If that character type is number, randomly generate number between 0 and 9
+    If that char type is a symbol, randomly generate number between 0 and y where y is number of symbols and return 
+Add that character to the password string
   
 
 
