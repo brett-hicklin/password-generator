8096fa5b33ab439b70b87f802bf996f28ec882d2
Brett Hicklin <hicklin.brett@gmail.com>
2023-01-16 20:00:51 -0500

    got passwordgenerate function working properly, added return statement on generate password function.

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,7 @@
 // goes to HTML doc and finds the generate ID inside of button
 var generateBtn = document.querySelector("#generate");
 var alphabet = ['a', 'b', 'c', 'd' ,'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-var symobols = ['!','"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
+var symbols = ['!','"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
 
 
 
@@ -36,13 +36,7 @@ function getRandomInt(max){
 function generatePassword(){
   var passwordLength = 8;
   var passwordRequirementsCompleted = [];
-  /* create an empty array to track requirements met
-  each time we go into an if block below we'll push that met requirement 
-  after looping is done and passwordString is complete now we compare the requirements given array to the 
-  requirements met array 
-  for each item in the passwordsrequirementsgiven 
-  check that it exists in passwordRequirementsCompleted
-  re-run loop */ 
+  
   var passwordRequirementsGiven = getpasswordRequirementsGiven();
   // var passwordRequirementsGiven = ['lowercase']
   var passwordString = ''
@@ -67,17 +61,18 @@ function generatePassword(){
       
     } else if (passwordRequirementsGiven[passwordReqIndex] === 'specialChar'){
       passwordRequirementsCompleted.push('specialChar')
-      var specialCharIndex = getRandomInt(symobols.length)
-      passwordString = passwordString + symobols[specialCharIndex]
+      var specialCharIndex = getRandomInt(symbols.length)
+      passwordString = passwordString + symbols[specialCharIndex]
     }
 
   console.log(passwordString)
   }
    for (let i = 0; i < passwordRequirementsGiven.length; i++){
     if (!passwordRequirementsCompleted.includes(passwordRequirementsGiven[i])){
-      generatePassword();
+      return generatePassword();
     }
-   }  
+   }
+   console.log('before return',passwordString )  
   return passwordString
 
 }
@@ -85,8 +80,10 @@ function generatePassword(){
 
 // Write password to the #password input
 function writePassword() {
-  // var password = generatePassword();
+  
+
   var password = generatePassword();
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -96,22 +93,10 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-/* prompt user for criteria
-  create password length prompt
-  create character prompt 
 
-  For each character in the password 
-Determine what type of character (lowercase, uppercase, number, symbol)
-Randomly select that type of character 
-    If that character type is lowercase or uppercase, generate random num between 0-25, gran associated letter
-        If lowercase call toLowercase
-        If uppercase call toUppercase
-    If that character type is number, randomly generate number between 0 and 9
-    If that char type is a symbol, randomly generate number between 0 and y where y is number of symbols and return 
-Add that character to the password string
+
   
 
 
 
 
-*/
